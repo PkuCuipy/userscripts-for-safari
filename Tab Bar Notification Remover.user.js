d2ee622bebb9bb9a6d435de14fcbf1ef1528dcdb
PkuCuipy <1900017785@pku.edu.cn>
2024-02-21 22:06:31 +0800

    `tab-bar-notification-remover` add support for youtube

--- Tab Bar Notification Remover.user.js
+++ Tab Bar Notification Remover.user.js
@@ -3,6 +3,7 @@
 // @description 知乎和 CSDN 在登录账户后, 如果有新通知, 则会在标签栏的标题中作为前缀显示. 这个脚本可以检测到这类前缀, 并自动去除它.
 // @match       *://blog.csdn.net/*
 // @match       *://www.zhihu.com/*
+// @match       *://www.youtube.com/*
 // update-time  2022-09-25, 2022-10-08
 // @inject-into content
 // ==/UserScript==
@@ -49,4 +50,23 @@ if (window.location.hostname.match(/zhihu\.com/) !== null) {
     }, 30_000);
 }
 
+
+if (window.location.hostname.match(/youtube\.com/) !== null) {
+    let id = setInterval(() => {
+        // 去除 YouTube 类似 "(39)" 的消息前缀
+        console.log("尝试去除 YouTube 类似 (39) 的消息前缀");
+        let curr_title = document.title;
+        let latent_prefix = curr_title.match(/\(\d+\) /);
+        console.log(latent_prefix);
+        if (latent_prefix !== null) {
+            latent_prefix = latent_prefix[0];
+            let new_title = curr_title.replace(latent_prefix, "");
+            document.title = new_title;
+            clearInterval(id);
+        }
+    }, 1000);
+    setTimeout(() => {    // 最多等 30s 加载
+       clearInterval(id);
+    }, 30_000);
+}
 
